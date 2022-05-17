e355738ea2a751492a7af16fe158a4095aeece78
Santiago Sánchez Esquivel <santiagosancheze8.1@gmail.com>
2022-05-17 17:20:38 -0500

    Hacer que se limpie la consola al escribir la palabra salir

--- dummy_shell.c
+++ dummy_shell.c
@@ -6,25 +6,39 @@
 
 #define MAX 100
 
-int main(int argc, char* argv[]) {
-  char **vector_cadenas;
-  char cadena[MAX];
+int main(int argc, char *argv[])
+{
+    char **vector_cadenas;
+    char cadena[MAX];
 
-  while (1) {
-          pid_t pid;
-	  printf("> ");
-	  leer_de_teclado(MAX, cadena);
-	  vector_cadenas = de_cadena_a_vector(cadena);
-          if (strcmp("salir",vector_cadenas[0]) == 0) break;
-	  pid = fork();
-          assert(pid >= 0);
-          if (pid == 0) {
-	    execvp(vector_cadenas[0], vector_cadenas);
-          } else {
+    while (1)
+    {
+        pid_t pid;
+        printf("> ");
+        leer_de_teclado(MAX, cadena);
+        vector_cadenas = de_cadena_a_vector(cadena);
+
+        if (strcmp("salir", vector_cadenas[0]) == 0){
+
+printf("Gracias por usar mi dummy shell ;-)\n");
+
+char * const args[] = {"clear", NULL}
+execvp(args[0], args);
+break;
+        }
+            
+        pid = fork();
+        assert(pid >= 0);
+        if (pid == 0)
+        {
+            execvp(vector_cadenas[0], vector_cadenas);
+        }
+        else
+        {
             wait(NULL);
-          }
-  }
-  printf("Gracias por usar mi dummy shell ;-)\n");
+        }
+    }
+    
 
-  return 0;
+    return 0;
 }
